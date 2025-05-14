072551478de60857abcf535dbf478e892102fe99
Naeim <naeim.smz@gmail.com>
2025-05-14 11:13:09 +0430

    useReducer initial set

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -2,7 +2,9 @@ import { useReducer, useState } from "react";
 
 function reducer(state, action) {
   console.log(state, action);
-  return state + action;
+  if (action.type === "inc") return state + action.payload;
+  if (action.type === "dec") return state - action.payload;
+  if (action.type === "setCount") return action.payload;
 }
 
 function DateCounter() {
@@ -16,17 +18,20 @@ function DateCounter() {
   date.setDate(date.getDate() + count);
 
   const dec = function () {
+    dispatch({ type: "dec", payload: -1 });
     // setCount((count) => count - 1);
     // setCount((count) => count - step);
   };
 
   const inc = function () {
+    dispatch({ type: "inc", payload: 1 });
     // setCount((count) => count + 1);
     // setCount((count) => count + step);
-    dispatch(1);
   };
 
   const defineCount = function (e) {
+    dispatch({ typeof: "setCount", payload: Number(e.target.value) });
+    // dispatch({})
     // setCount(Number(e.target.value));
   };
 
