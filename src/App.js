bac92a3c77e53c2fb50ea441d6c1e3ec625b2df3
Naeim <naeim.smz@gmail.com>
2025-05-16 01:20:45 +0430

    displaying questions

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const initialState = {
 
   // 'loading'. 'error', 'ready', 'active', 'finished'
   status: "loading",
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -30,7 +31,10 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numQuestions = questions.length;
 
@@ -51,7 +55,7 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question />}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
--- src/Question.js
+++ src/Question.js
@@ -1,5 +1,15 @@
-function Question() {
-  return <div>Question</div>;
+import Options from "./Options";
+
+function Question({ question }) {
+  console.log(question);
+
+  return (
+    <div>
+      <h4>{question.question}</h4>
+
+      <Options question={question} />
+    </div>
+  );
 }
 
 export default Question;
